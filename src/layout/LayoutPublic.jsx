3d6dab4fb11bf2bb941a9bb86f37da6053b32331
SteveO <orregoe@gmail.com>
2023-03-25 15:29:52 -0300

    creacion de navbar y footer

--- src/layout/LayoutPublic.jsx
+++ src/layout/LayoutPublic.jsx
@@ -1,21 +1,17 @@
 import { Outlet } from "react-router-dom"
-import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+import NavBar from "../components/NavBar"
+
 
 const LayoutPublic = () => {
     return (
         <>
-            <div className="bg-slate-300 text-black h-screen flex text-white">
-                <Navbar />
-                <hr />
-                <br />
+            <div>
+                <NavBar />
                 <main>
                     <Outlet />
                 </main>
-                <br />
-                <hr />
-                <footer>
-                    #Footer
-                </footer>
+                <Footer />
             </div>
         </>
     )
